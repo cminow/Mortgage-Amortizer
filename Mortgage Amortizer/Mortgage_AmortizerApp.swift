ea78eb2f13bfde679b66ca793493d88f4b870258
Charlie Minow <minow@mac.com>
2025-11-11 14:55:39 -0700

    Initial Commit

---
+++ Mortgage Amortizer/Mortgage_AmortizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mortgage_AmortizerApp.swift
+//  Mortgage Amortizer
+//
+//  Created by Charlie Minow on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Mortgage_AmortizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mortgage AmortizerTests/Mortgage_AmortizerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Mortgage_AmortizerTests.swift
+//  Mortgage AmortizerTests
+//
+//  Created by Charlie Minow on 11/11/25.
+//
+
+import Testing
+@testable import Mortgage_Amortizer
+
+struct Mortgage_AmortizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
