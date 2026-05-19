b252c0ce3cb68a51d7687d313c98494ab98e819a
Charlie Minow <minow@mac.com>
2026-05-19 08:30:40 -0600

    Close the app when the last window goes away.

--- Mortgage Amortizer/Mortgage_AmortizerApp.swift
+++ Mortgage Amortizer/Mortgage_AmortizerApp.swift
@@ -9,11 +9,18 @@ import SwiftUI
 
 @main
 struct Mortgage_AmortizerApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     var body: some Scene {
         WindowGroup("Mortgage", for: Mortgage.self) { $mortgageId in
             AmortizationView()
         }
         
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
     }
 }
